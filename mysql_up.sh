f00b52a1413829479580e89309a8b07a7a5ef1f7
Jason Daynes <daynesj@hotmail.com>
2026-01-08 21:19:21 -0400

    Changes made to index.html

--- mysql_up.sh
+++
@@ -1,2 +0,0 @@
-docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=projects -e MYSQL_USER=admin -e MYSQL_PASSWORD=password -d mysql:9.5
-
--- phpmyadmin_up.sh
+++
@@ -1,1 +0,0 @@
-docker run --name phpmyadmin -d --link mysql:db -p 8081:80 phpmyadmin:5.2.3-apache
